1e483d117d0615cc1a16287c0a4956d092a7e7a3
Dhruv <pd1900565@gmail.com>
2026-04-11 12:14:05 +0530

    Delete DeveloperKnowledgeBase/Backend directory

--- DeveloperKnowledgeBase/Backend/models/note.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, Text
-from db.session import Base
-
-class Note(Base):
-
-    __tablename__ = "notes"
-
-    id = Column(Integer, primary_key=True, index=True)
-    content = Column(Text)
--- DeveloperKnowledgeBase/Backend/schemas/note.py
+++
@@ -1,7 +0,0 @@
-#  Use when you use script.js in frontend aslo change in notes.py at line 20
-
-from pydantic import BaseModel
-
-class Notes(BaseModel):
-
-    content: str
