d262bc5bfee2bb3086651cd47c484169fb978306
Dhruv-Cmds <pd1900565@gmail.com>
2026-04-24 16:25:22 +0530

    improved system design

--- DeveloperKnowledgeBase/Backend/schemas/note.py
+++ DeveloperKnowledgeBase/Backend/schemas/note.py
@@ -1,7 +1,13 @@
 #  Use when you use script.js in frontend aslo change in notes.py at line 20
 
-from pydantic import BaseModel
+from pydantic import BaseModel, StringConstraints
+from typing import Annotated
 
 class Notes(BaseModel):
 
-    content: str
+    content: Annotated[str,
+                       StringConstraints
+                       (min_length=10,
+                        max_length=255
+                        )
+                    ]
